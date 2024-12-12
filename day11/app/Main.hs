92a937e4fe75e2ed84998197f81ba2cc8d53ea5c
Christoffer Nilsson <christoffer.nilsson@svenskaspel.se>
2024-12-12 08:25:54 +0100

    day11 part2

--- day11/app/Main.hs
+++ day11/app/Main.hs
@@ -1,40 +1,47 @@
 import AoCIO
+import Data.List (groupBy, sortBy)
 import Data.List.Split (splitOn)
 import Log (verbosePrint)
+import Data.Function (on)
 
 -- split number into two parts with half of the digits in each part
 splitDigit :: String -> [Int]
 splitDigit n = let (a, b) = splitAt (length n `div` 2) n in [read a, read b]
 
-iteration :: Int -> [Int]
-iteration n | n == 0 = [1]
-            | even (length (show n)) = splitDigit $ show n
-            | otherwise = [n * 2024]
+iteration :: (Int, Int) -> [(Int, Int)]
+iteration (n, c)  | n == 0 = [(1, c)]
+                  | even (length (show n)) = map (\x -> (x, c)) $ splitDigit $ show n
+                  | otherwise = [(n * 2024, c)]
 
-groupAndCount :: [Int] -> [(Int, Int)]
-groupAndCount [] = []
-groupAndCount (x:xs) = (x, length $ takeWhile (== x) (x:xs)) : groupAndCount (dropWhile (== x) xs)
+mergeCounts :: [(Int, Int)] -> [(Int, Int)]
+mergeCounts pairs =
+  map mergeGroup grouped
+  where
+    sorted = sortBy (compare `on` fst) pairs
+    grouped = groupBy ((==) `on` fst) sorted
+    mergeGroup grp = (fst (head grp), sum (map snd grp))
 
-nextArray :: [Int] -> [Int]
-nextArray x = x >>= iteration
+nextArray :: [(Int, Int)] -> [(Int, Int)]
+nextArray x = mergeCounts(x >>= iteration)
 
 main :: IO ()
 main = do
   input <- readInput "input.txt"
   let rocks = readIntList $ splitOn " " input
+  let rockCounts = map (\x -> (x, 1)) rocks
   verbosePrint "Rocks"
-  verbosePrint rocks
+  verbosePrint rockCounts
 
-  let iteration1 = rocks >>= iteration
+  let iteration1 = rockCounts >>= iteration
   verbosePrint "Iteration 1"
   verbosePrint iteration1
 
-  let iteration25 = iterate nextArray rocks !! 25
+  let iteration25 = iterate nextArray rockCounts !! 25
 
-  let part1 = length iteration25
+  let part1 = sum $ map snd iteration25
   print ("Part 1: " ++ show part1)
 
 
-  let iteration75 = iterate nextArray rocks !! 75
-  let part2 = length iteration75
+  let iteration75 = iterate nextArray rockCounts !! 75
+  let part2 = sum $ map snd iteration75
   print ("Part 2: " ++ show part2)
